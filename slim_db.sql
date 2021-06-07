8f891762da5086539ef45846129a22cfb0526a00
mmmsafry <mmmsafry@gmail.com>
2021-06-07 19:58:50 +0530

    model changes and sql dump as per response.js

--- slim_db.sql
+++ slim_db.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 02/06/2021 22:32:13
+ Date: 07/06/2021 19:58:07
 */
 
 SET NAMES utf8mb4;
@@ -24,24 +24,25 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0',
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `count` int(11) NULL DEFAULT 0,
   `created_at` timestamp(0) NULL DEFAULT NULL,
   `updated_at` timestamp(0) NULL DEFAULT NULL,
   `deleted_at` timestamp(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of categories
 -- ----------------------------
-INSERT INTO `categories` VALUES (1, '0', 'root Element', NULL, '2021-06-02 15:33:57', NULL);
-INSERT INTO `categories` VALUES (2, '0', 'seondRoot', NULL, '2021-06-02 15:33:57', NULL);
-INSERT INTO `categories` VALUES (3, '1', 'sub 1', NULL, '2021-06-02 15:33:57', NULL);
-INSERT INTO `categories` VALUES (4, '1', 'sub 2', NULL, '2021-06-02 15:33:57', NULL);
-INSERT INTO `categories` VALUES (5, '2', 'sub 1 of 2', NULL, '2021-06-02 15:33:57', NULL);
-INSERT INTO `categories` VALUES (6, '2', 'sub 2 of 2', NULL, '2021-06-02 16:52:06', '2021-06-02 16:52:06');
-INSERT INTO `categories` VALUES (7, '3', 'sub sub category ', NULL, '2021-06-02 16:52:06', '2021-06-02 16:52:06');
-INSERT INTO `categories` VALUES (9, '0', 'category root 3', '2021-06-02 16:53:20', '2021-06-02 16:53:20', NULL);
+INSERT INTO `categories` VALUES (1, '0', 'root Element', 137, NULL, '2021-06-02 15:33:57', NULL);
+INSERT INTO `categories` VALUES (2, '0', 'seondRoot', 50, NULL, '2021-06-02 15:33:57', NULL);
+INSERT INTO `categories` VALUES (3, '1', 'sub 1', 75, NULL, '2021-06-02 15:33:57', NULL);
+INSERT INTO `categories` VALUES (4, '1', 'sub 2', 35, NULL, '2021-06-02 15:33:57', NULL);
+INSERT INTO `categories` VALUES (5, '2', 'sub 1 of 2', 60, NULL, '2021-06-02 15:33:57', NULL);
+INSERT INTO `categories` VALUES (6, '2', 'sub 2 of 2', 10, NULL, '2021-06-03 16:52:03', NULL);
+INSERT INTO `categories` VALUES (7, '3', 'sub sub category ', 6, NULL, '2021-06-03 16:52:03', NULL);
+INSERT INTO `categories` VALUES (9, '0', 'category root 3', 56, '2021-06-02 16:53:20', '2021-06-02 16:53:20', NULL);
 
 -- ----------------------------
 -- Table structure for users
@@ -55,7 +56,7 @@ CREATE TABLE `users`  (
   `created_at` timestamp(0) NULL DEFAULT NULL,
   `updated_at` timestamp(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
